644dc9ebd43ad457b96f9c62c40318c50f7bbb84
Saibal Talukder <akashts724@gmail.com>
2023-06-17 12:27:37 +0600

    Project Setup

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -31,6 +31,8 @@ $routes->set404Override();
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
 
+//Language Settings
+$routes->get('language/setting', 'SettingController::index');
 /*
  * --------------------------------------------------------------------
  * Additional Routing
