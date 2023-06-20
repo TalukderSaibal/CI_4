6e94cda4fa83e468ef2ec6c072ce3953699e503a
Saibal Talukder <akashts724@gmail.com>
2023-06-20 18:36:41 +0600

    Working On project

--- app/Views/setting/createLanguage.php
+++ app/Views/setting/createLanguage.php
@@ -1,84 +1,64 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/x-icon" href="<?= base_url('icon/favicon.ico') ?>" />
-    <title>Document</title>
+<?= $this->extend('layout/base') ?>
 
-    <!-- Font Awesome CDN Link Here -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
+<?= $this->section('content') ?>
 
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" >
-
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css">
-
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js"></script>
-
-    <link rel="stylesheet" href="<?= base_url('css/style.css') ?>">
-</head>
-<body>
-    <div class="container">
-        <div class="row">
-            <h1>Add language</h1>
-            <div class="language_form">
-                <form action="<?= base_url('language/create') ?>" method="post" enctype="multipart/form-data" id="myForm">
-                    <div class="file_div">
-                        <div class="form-group frm">
-                            <label for="exampleFormControlFile1">Choose Flag</label>
-                            <input type="file" name="flag" class="form-control-file">
-                            <span>Allowed Type : (png.jpg,jpeg)</span>
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Name * </label>
-                        <input type="Text" class="form-control" id="name" name="name" value="<?php echo old('name'); ?>">
-                        <span style="color:red;" id="nameErr"></span>
-                        <span style="color:red;"><?= validation_show_error('name') ?></span>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="exampleFormControlSelect1">Code : *</label>
-                        <select class="form-control" id="myDropdown" name="code">
-                            <option value="">Select Code</option>
-                            <?php
-                                
-                                foreach($res as $val){ ?>
-                                    <?php
-                                    
-                                        $codeName = $val->country_name . '(' . $val->country_code . ')';
-                                    
-                                    ?>
-                                    <option value="<?= $val->id ?>"><?= $codeName ?></option>
-                                <?php }
-                                
-                                ?>
-                        </select>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="exampleFormControlSelect1">Direction * : </label>
-                        <select class="form-control" name="direction">
-                            <option value="LTR">LTR</option>
-                            <option value="RTL">RTL</option>
-                        </select>
-                    </div>
-
-                    <div class="form-check">
-                        <input type="checkbox" class="form-check-input" name="languageCheck" id="languageCheck">
-                        <label class="form-check-label">Default Language</label>
-                        <span style="color:red;"><?= validation_show_error('name') ?></span>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </form>
+<div class="form_flex11">
+    <h1>Add language</h1>
+    <div class="language_form">
+        <form action="<?= base_url('language/create') ?>" method="post" enctype="multipart/form-data" id="myForm">
+            <div class="file_div">
+                <div class="form-group frm">
+                    <label for="exampleFormControlFile1">Choose Flag</label>
+                    <input type="file" name="flag" class="form-control-file">
+                    <span>Allowed Type : (png.jpg,jpeg)</span>
+                </div>
             </div>
-        </div>
-    </div>
 
+            <div class="form-group">
+                <label for="exampleInputEmail1">Name * </label>
+                <input type="Text" class="form-control" id="name" name="name" value="<?php echo old('name'); ?>">
+                <span style="color:red;" id="nameErr"></span>
+                <span style="color:red;"><?= validation_show_error('name') ?></span>
+            </div>
+
+            <div class="form-group">
+                <label for="exampleFormControlSelect1">Code : *</label>
+                <select class="form-control" id="myDropdown" name="code">
+                    <option value="">Select Code</option>
+                    <?php
+                        
+                        foreach($res as $val){ ?>
+                            <?php
+                            
+                                $codeName = $val->country_name . '(' . $val->country_code . ')';
+                            
+                            ?>
+                            <option value="<?= $val->id ?>"><?= $codeName ?></option>
+                        <?php }
+                        
+                        ?>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label for="exampleFormControlSelect1">Direction * : </label>
+                <select class="form-control" name="direction">
+                    <option value="LTR">LTR</option>
+                    <option value="RTL">RTL</option>
+                </select>
+            </div>
+
+            <div class="form-check">
+                <input type="checkbox" class="form-check-input" name="languageCheck" id="languageCheck">
+                <label class="form-check-label">Default Language</label>
+                <span style="color:red;"><?= validation_show_error('name') ?></span>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</div>
+
+<?= $this->endSection('content') ?>
 
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
@@ -117,5 +97,3 @@
             $('#myForm').unbind('submit').submit();
         })
 </script>
-</body>
-</html>
--- app/Views/setting/language.php
+++ app/Views/setting/language.php
@@ -1,46 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/x-icon" href="<?= base_url('icon/favicon.ico') ?>" />
-    <title>Document</title>
+<?= $this->extend('layout/base') ?>
 
-    <!-- Font Awesome CDN Link Here -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
+<?= $this->section('content') ?>
 
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" >
+<div class="form_flex11">
+<div class="language_header">
+    <div class="lan_head">
+        <h1>Language</h1>
+    </div>
 
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css">
+        <div class="icon_category">
+            <a href="">
+                Back
+            </a>
+            <a href="">
+                Settings
+            </a>
+            <a href="<?= base_url('language/create') ?>">
+                <i class="fa fa-plus" aria-hidden="true"></i>
+            </a>
+        </div>
 
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js"></script>
-
-    <link rel="stylesheet" href="<?= base_url('css/style.css') ?>">
-</head>
-<body>
-    <div class="container">
-        <div class="row">
-            <div class="language_header">
-                <div class="lan_head">
-                    <h1>Language</h1>
-                </div>
-
-                <div class="icon_category">
-                    <a href="">
-                        Back
-                    </a>
-                    <a href="">
-                        Settings
-                    </a>
-                    <a href="<?= base_url('language/create') ?>">
-                        <i class="fa fa-plus" aria-hidden="true"></i>
-                    </a>
-                </div>
-
-            </div>
+    </div>
 
             <?php
 
@@ -127,7 +107,7 @@
                 </div>
                 </div>
             </div>
-        </div>
     </div>
-</body>
-</html>
+
+
+<?= $this->endSection('content') ?>
